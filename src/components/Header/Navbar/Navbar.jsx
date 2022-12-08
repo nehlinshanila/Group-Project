59826284e4dfe9be8361e039d008184b8103a343
Shanila Nehlin <nehlinshanila@gmail.com>
2022-12-08 16:28:54 +0600

    tests done

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import HomeIcon from "./homeIcon/HomeIcon";
 import Info from "../info/info";
+import { useState } from "react";
 const Navbar = () => {
-    
+    const isActive = false;
   return (
     <>
       {/* <HomeIcon />
       <HomeIcon />*/}
+      {isActive === true ? <HomeIcon /> : <h1>didnt happen</h1>}
       <HomeIcon />
 
     <Info />
